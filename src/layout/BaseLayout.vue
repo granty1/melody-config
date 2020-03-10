708ff9d039b86393430672d1ac6805f25555ef87
wangzhen <1046060018@qq.com>
2020-03-10 23:59:55 +0800

    init

---
+++ src/layout/BaseLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- 头部 -->
+    <div></div>
+    <!-- 主体部分 -->
+    <div></div>
+    <!-- 尾部 -->
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseLayout',
+}
+</script>
+
+<style scoped></style>
