f63a691d69932ccafd4b6bd25683197488e0642c
Granty1 <granty1@163.com>
2020-03-20 17:49:23 +0800

    Modify the display method of logger config card

--- src/views/logging/index.vue
+++ src/views/logging/index.vue
@@ -16,108 +16,115 @@
                   Melody提供了比较完善的logger。
                 </div>
               </el-form-item>
-              <el-row type="flex" class="row-bg" justify="space-around">
-                <el-col :span="11">
-                  <!-- Level -->
-                  <el-form-item label="Level">
-                    <el-select
-                      :disabled="!logging.baseLoggerEnable"
-                      v-model="logging.base.level"
-                      @change="updateLogging"
-                      placeholder="选择log级别"
-                    >
-                      <el-option v-for="item in logLevels" :key="item" :label="item" :value="item">
-                      </el-option>
-                    </el-select>
-                    <div style="font-size: 12px">
-                      日志打印的级别。
-                    </div>
-                  </el-form-item>
-                </el-col>
-                <!-- Prefix -->
-                <el-col :span="11" :offset="2">
-                  <el-form-item label="Prefix">
-                    <el-input
-                      :disabled="!logging.baseLoggerEnable"
-                      v-model="logging.base.prefix"
-                      @input="updateLogging"
-                    ></el-input>
-                    <div style="font-size: 12px">
-                      在日志行之前添加字符串前缀。
-                    </div>
-                  </el-form-item>
-                </el-col>
-              </el-row>
-              <el-row type="flex" class="row-bg" justify="space-around">
-                <!-- Message Format -->
-                <el-col :span="logging.enableCustomFormat ? 11 : 24">
-                  <el-form-item label="Message Format">
-                    <el-select
-                      @change="messageFormatHandle"
-                      :disabled="!logging.baseLoggerEnable"
-                      v-model="logging.base.format"
-                      placeholder="选择日志格式"
-                    >
-                      <el-option v-for="item in formats" :key="item" :label="item" :value="item">
-                      </el-option>
-                    </el-select>
-                    <div style="font-size: 12px">
-                      日志记录、打印的格式。
-                    </div>
-                  </el-form-item>
-                </el-col>
-                <!-- Custom Format -->
-                <el-col v-if="logging.enableCustomFormat" :span="11" :offset="2">
-                  <el-form-item label="Custom log format">
-                    <el-input
-                      @input="updateLogging"
-                      v-model="logging.base.custom"
-                      placeholder="%{message}"
-                    ></el-input>
-                    <div style="font-size: 12px">
-                      可以使用一些表达式，更多表达式请参考<a
-                        target="blank"
-                        href="https://github.com/op/go-logging/blob/master/format.go#L156"
-                        >go-logging</a
+              <template v-if="logging.baseLoggerEnable">
+                <el-row type="flex" class="row-bg" justify="space-around">
+                  <el-col :span="11">
+                    <!-- Level -->
+                    <el-form-item label="Level">
+                      <el-select
+                        :disabled="!logging.baseLoggerEnable"
+                        v-model="logging.base.level"
+                        @change="updateLogging"
+                        placeholder="选择log级别"
                       >
-                    </div>
-                  </el-form-item>
-                </el-col>
-              </el-row>
-              <!-- Log Example -->
-              <el-row v-if="logging.base.format != 'custom'">
-                <el-col :span="24">
-                  示例：<code>{{ logging.exampleLog }}</code>
-                </el-col>
-              </el-row>
-              <!-- Log stash -->
-              <el-form-item label="LogStash">
-                <el-switch
-                  :disabled="!logging.baseLoggerEnable"
-                  @change="enableLogStash"
-                  v-model="logging.logstashEnable"
-                  >Enable</el-switch
-                >
-                <div style="font-size: 12px">选择开启是否<code>LogStash</code></div>
-              </el-form-item>
-              <!-- Writers -->
-              <el-form-item label="Writers">
-                <el-checkbox
-                  @change="updateLogging"
-                  :disabled="!logging.baseLoggerEnable"
-                  v-model="logging.base.syslog"
-                  >Syslog</el-checkbox
-                >
-                <el-checkbox
-                  @change="updateLogging"
-                  :disabled="!logging.baseLoggerEnable"
-                  v-model="logging.base.stdout"
-                  >Stdout</el-checkbox
-                >
-                <div style="font-size: 12px">
-                  <code>Syslog</code>将日志写入syslog系统。 <code>Stdout</code>将日志写入控制台。
-                </div>
-              </el-form-item>
+                        <el-option
+                          v-for="item in logLevels"
+                          :key="item"
+                          :label="item"
+                          :value="item"
+                        >
+                        </el-option>
+                      </el-select>
+                      <div style="font-size: 12px">
+                        日志打印的级别。
+                      </div>
+                    </el-form-item>
+                  </el-col>
+                  <!-- Prefix -->
+                  <el-col :span="11" :offset="2">
+                    <el-form-item label="Prefix">
+                      <el-input
+                        :disabled="!logging.baseLoggerEnable"
+                        v-model="logging.base.prefix"
+                        @input="updateLogging"
+                      ></el-input>
+                      <div style="font-size: 12px">
+                        在日志行之前添加字符串前缀。
+                      </div>
+                    </el-form-item>
+                  </el-col>
+                </el-row>
+                <el-row type="flex" class="row-bg" justify="space-around">
+                  <!-- Message Format -->
+                  <el-col :span="logging.enableCustomFormat ? 11 : 24">
+                    <el-form-item label="Message Format">
+                      <el-select
+                        @change="messageFormatHandle"
+                        :disabled="!logging.baseLoggerEnable"
+                        v-model="logging.base.format"
+                        placeholder="选择日志格式"
+                      >
+                        <el-option v-for="item in formats" :key="item" :label="item" :value="item">
+                        </el-option>
+                      </el-select>
+                      <div style="font-size: 12px">
+                        日志记录、打印的格式。
+                      </div>
+                    </el-form-item>
+                  </el-col>
+                  <!-- Custom Format -->
+                  <el-col v-if="logging.enableCustomFormat" :span="11" :offset="2">
+                    <el-form-item label="Custom log format">
+                      <el-input
+                        @input="updateLogging"
+                        v-model="logging.base.custom"
+                        placeholder="%{message}"
+                      ></el-input>
+                      <div style="font-size: 12px">
+                        可以使用一些表达式，更多表达式请参考<a
+                          target="blank"
+                          href="https://github.com/op/go-logging/blob/master/format.go#L156"
+                          >go-logging</a
+                        >
+                      </div>
+                    </el-form-item>
+                  </el-col>
+                </el-row>
+                <!-- Log Example -->
+                <el-row v-if="logging.base.format != 'custom'">
+                  <el-col :span="24">
+                    示例：<code>{{ logging.exampleLog }}</code>
+                  </el-col>
+                </el-row>
+                <!-- Log stash -->
+                <el-form-item label="LogStash">
+                  <el-switch
+                    :disabled="!logging.baseLoggerEnable"
+                    @change="enableLogStash"
+                    v-model="logging.logstashEnable"
+                    >Enable</el-switch
+                  >
+                  <div style="font-size: 12px">选择开启是否<code>LogStash</code></div>
+                </el-form-item>
+                <!-- Writers -->
+                <el-form-item label="Writers">
+                  <el-checkbox
+                    @change="updateLogging"
+                    :disabled="!logging.baseLoggerEnable"
+                    v-model="logging.base.syslog"
+                    >Syslog</el-checkbox
+                  >
+                  <el-checkbox
+                    @change="updateLogging"
+                    :disabled="!logging.baseLoggerEnable"
+                    v-model="logging.base.stdout"
+                    >Stdout</el-checkbox
+                  >
+                  <div style="font-size: 12px">
+                    <code>Syslog</code>将日志写入syslog系统。 <code>Stdout</code>将日志写入控制台。
+                  </div>
+                </el-form-item>
+              </template>
             </melody-card-item>
           </melody-card>
         </el-col>
