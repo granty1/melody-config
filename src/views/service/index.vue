b6c1c833b1f80cab770423fb252f955df0aa5839
wangzhen <1046060018@qq.com>
2020-03-21 20:13:44 +0800

    update service ui

--- src/views/service/index.vue
+++ src/views/service/index.vue
@@ -10,31 +10,65 @@
       <el-row type="flex" class="row-bg" justify="space-around">
         <!-- 左侧 -->
         <el-col :span="11" class="container">
-          <!-- Service Name -->
           <melody-card>
+            <!-- Service Name -->
             <melody-card-item title="Service Name">
               <el-form-item label="Name">
-                <el-input
-                  v-model="config.name"
-                  @input="save"
-                  placeholder="请输入您的Service Name"
-                ></el-input>
-                <div style="font-size: 12px">
+                <el-input v-model="config.name" @input="save" placeholder="My Service"></el-input>
+                <div class="fs12">
                   友好的名称，标题，日期，版本或任何其他简短描述，可帮助您在打开时识别JSON文件。
                 </div>
               </el-form-item>
             </melody-card-item>
             <!-- Available hosts -->
             <melody-card-item title="Available hosts">
-              <el-form-item label="address">
+              <el-form-item>
+                <div class="fs12">
+                  在此处添加Melody用于检索数据的所有地址，以及是否由服务发现解决的地址：
+                </div>
+                <el-radio-group v-model="sdType" size="small">
+                  <el-radio-button label="Static address resolution">
+                    Static address resolution
+                  </el-radio-button>
+                  <el-radio-button label="Etcd" :disabled="etcdDisabled">
+                    Etcd
+                  </el-radio-button>
+                  <el-radio-button label="DNS SRV">
+                    DNS SRV
+                  </el-radio-button>
+                  <el-radio-button label="Custom service discovery">
+                    Custom service discovery
+                  </el-radio-button>
+                </el-radio-group>
                 <el-input
+                  style="margin-top: 10px"
                   v-model="config.address"
                   @input="save"
-                  placeholder="请输入您的Available hosts"
-                ></el-input>
-                <div style="font-size: 12px">
-                  The address where KrakenD connects to. Some valid examples can be https://myapi,
-                  amqp://host, 192.0.2.1:25, my.service.tld.
+                  placeholder="address"
+                >
+                  <template slot="prepend">
+                    <el-checkbox v-model="disableSanitize">disable sanitize</el-checkbox>
+                  </template>
+                </el-input>
+                <div class="fs12">
+                  Melody连接到的地址。一些有效的示例:
+                  <code>https://myapi</code>，<code>amqp://host</code>，
+                  <code>192.0.2.1:25</code>，<code>my.service.tld</code>。
+                </div>
+                <div class="fs12">
+                  当不需要为协议检查地址字符串时，请选择disable sanitize。
+                </div>
+              </el-form-item>
+            </melody-card-item>
+            <!-- Timeouts and TTL -->
+            <melody-card-item title="Timeouts and TTL">
+              <div class="sf12">
+                除非在每个endpoint中明确覆盖，否则以下所有设置将在所有backend中使用
+              </div>
+              <el-form-item label="Backend Timeout" prop="time">
+                <el-input v-model="config.timeout" @input="save" placeholder="3000ms"></el-input>
+                <div class="fs12">
+                  友好的名称，标题，日期，版本或任何其他简短描述，可帮助您在打开时识别JSON文件。
                 </div>
               </el-form-item>
             </melody-card-item>
@@ -54,20 +88,20 @@
                   placeholder="8080"
                   autocomplete="off"
                 ></el-input>
-                <div style="font-size: 12px">
+                <div class="fs12">
                   Melody用来监听连接的端口，默认为<code>8080</code>。
                   也可以在启动时使用标志<code>-p</code>指定该端口。
                 </div>
               </el-form-item>
               <!-- HTTP Read Timeout -->
-              <el-form-item label="HTTP Read Timeout" prop="read_timeout">
+              <el-form-item label="HTTP Read Timeout" prop="time">
                 <el-input
                   @input="save"
                   v-model="config.read_timeout"
                   placeholder="0s"
                   autocomplete="off"
                 ></el-input>
-                <div style="font-size: 12px">
+                <div class="fs12">
                   读取整个HTTP请求（包括正文）的最大持续时间。
                 </div>
               </el-form-item>
@@ -84,7 +118,7 @@ import MelodyCard from '@/components/MelodyCard'
 import MelodyCardItem from '@/components/MelodyCardItem'
 import { validTimeDuration, validNumber } from '@/utils/regxp'
 
-let needCheckProps = ['name', 'port', 'read_timeout']
+let needCheckProps = ['name', 'port', 'time']
 
 export default {
   name: 'Service',
@@ -99,8 +133,11 @@ export default {
       config: this.$ls.get('config'),
       serviceConfigRules: {
         port: [{ validator: validPort, trigger: 'blur' }],
-        read_timeout: [{ validator: validReadTimeout, trigger: 'blur' }],
+        time: [{ validator: validReadTimeout, trigger: 'blur' }],
       },
+      sdType: 'Static address resolution',
+      etcdDisabled: true,
+      disableSanitize: false,
     }
   },
   components: {
