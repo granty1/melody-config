13e2b20dc936d48792b29538a59c87f162d32fb5
Yuhn-z <1029363172@qq.com>
2020-04-05 18:40:43 +0800

    修复清除缓存后出现的bug

--- src/layout/BaseLayout.vue
+++ src/layout/BaseLayout.vue
@@ -52,6 +52,7 @@
 <script>
 import FileSaver from 'file-saver'
 import { Loading } from 'element-ui'
+import defaultSetting from '@/config/defaultSetting'
 export default {
   name: 'BaseLayout',
   data() {
@@ -90,9 +91,27 @@ export default {
         type: 'warning',
       })
         .then(() => {
-          this.$store.commit('updateServiceConfig', { version: 1 })
+          this.$store.commit('updateServiceConfig', {})
           this.$ls.remove('logging')
           this.$ls.remove('safe')
+
+          let config = this.$ls.get('config') === null ? {} : this.$ls.get('config')
+          config.version = 1
+          config.output_encoding =
+            config.output_encoding == undefined ? 'json' : config.output_encoding
+          config.disable_rest = config.disable_rest == undefined ? false : config.disable_rest
+          config.extra_config = config.extra_config == undefined ? {} : config.extra_config
+          this.$store.commit('updateServiceConfig', config)
+
+          let activeCards =
+            this.$ls.get('active_cards') === null
+              ? defaultSetting.defaultActiveCards
+              : this.$ls.get('active_cards')
+          this.$store.commit('updateActiveCards', activeCards)
+
+          let availableHosts =
+            this.$ls.get('availableHosts') === null ? [] : this.$ls.get('availableHosts')
+          this.$store.commit('setAvailableHosts', availableHosts)
           this.$router.push('/')
 
           this.$message({
